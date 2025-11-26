45b92598a47cadc40e23379dc175cf44397c4012
IbrahimCSKhader <ik2907951@gmail.com>
2025-11-26 20:03:58 +0200

    get users

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
                   <Link className="nav-link active" aria-current="page" to="/"> Ibrahim test  </Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
 
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/">
--- src/pages/user/Users.jsx
+++ src/pages/user/Users.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 export default function Users() {
+
+  const getUsers = async () => {
+    try {
+      console.log("BASE URL:", import.meta.env.VITE_BASE_URL);
+      const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/users`);
+      console.log("Users data:", response.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <div>Userssss</div>
+    <div>Users</div>
   );
-}   
+}
